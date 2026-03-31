6d67addfb8f7763bfefd351dafb4ffd16f897764
tesfay <tesfayhailemariam29@gmail.com>
2026-03-31 12:00:50 +0300

    handle the next upcoming user message

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const messageInput = document.querySelector(".message-input");
+const handleOutgoingMessage = (userMessage) => {
+    const messageContent = `<div class="message-text">&{userMessage}</div>`;
+    createMessageElement(messageContent, "user-message");
+}
 // handle enter key press for sending messages
 messageInput.addEventListener("keydown", (e) => {
     const userMessage = e.target.value.trim();
     if (e.key === "Enter" && userMessage) {
         console.log(userMessage);
+        handleOutgoingMessage(userMessage);
+
     }
 })

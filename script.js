6bf5fc8c7208a58ce43bddf42a7078c5557edb46
tesfay <tesfayhailemariam29@gmail.com>
2026-03-31 12:10:30 +0300

    creation of message element

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const messageInput = document.querySelector(".message-input");
+//create message element with dynamic classes and return it 
+const createMessageElement = (content, classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", classes);
+    div.innerHTML = content;
+    return div;
+}
 const handleOutgoingMessage = (userMessage) => {
     const messageContent = `<div class="message-text">&{userMessage}</div>`;
     createMessageElement(messageContent, "user-message");
